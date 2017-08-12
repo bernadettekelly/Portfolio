ef932c832a0fa6530de271a2f2b06263cc1fbc15
bernadettekelly <bernadettemkelly54@gmail.com>
2017-08-12 09:05:22 -0400

    added gallery

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
+
 $(document).ready(function() {
 	$('.Page').hide();
 	$('.Page1').show();
+	$('.portfolio').css("font-weight", "Bold");
 });
 
 $('.about').click(function(e) {
 	e.preventDefault();
 	$('.Page').hide();
 	$('.Page2').show();
+	$('.about').css("font-weight", "Bold");
+	$('.portfolio').css("font-weight", "normal");
 });
 
 $('.portfolio').click(function(e) {
 	e.preventDefault();
 	$('.Page').hide();
 	$('.Page1').show();
+	$('.portfolio').css("font-weight", "Bold");
+	$('.about').css("font-weight", "normal");
+});
+
+$('.next').click(function(e){
+ $('.gallery-wrapper').css("left", "-=600px");
+});
+
+$('.previous').click(function(e){
+ $('.gallery-wrapper').css("left", "+=600px");
 });
