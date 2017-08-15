20dafd4458ec287c572507a87f3191997b6840a9
bernadettekelly <bernadettemkelly54@gmail.com>
2017-08-14 21:25:17 -0400

    added animation

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 
+let left = 0;
+var tl = new TimelineLite();
+
 $(document).ready(function() {
 	$('.Page').hide();
 	$('.Page1').show();
 	$('.portfolio').css("font-weight", "Bold");
+	TweenLite.from('.Portfolio_Container', 1, {autoAlpha:0, delay:.5});
+	TweenMax.staggerFrom('header', 1, {scale:0, autoAlpha:0}, 0.2);
+	TweenMax.staggerFrom('.AboutPortfolio', 1, {scale:0, autoAlpha:0}, 0.2);
 });
 
 $('.about').click(function(e) {
@@ -11,6 +17,12 @@ $('.about').click(function(e) {
 	$('.Page2').show();
 	$('.about').css("font-weight", "Bold");
 	$('.portfolio').css("font-weight", "normal");
+	TweenMax.staggerFrom('header', 1, {scale:0, autoAlpha:0}, 0.2);
+	TweenMax.staggerFrom('.AboutPortfolio', 1, {scale:0, autoAlpha:0}, 0.2);
+	TweenLite.from('.1', 1, {autoAlpha:0, delay:.25});
+	TweenLite.from('.2', 1, {autoAlpha:0, delay:.5});
+	TweenLite.from('.3', 1, {autoAlpha:0, delay:1});
+	TweenLite.from('.About_Title', 1, {autoAlpha:0, delay:.5});
 });
 
 $('.portfolio').click(function(e) {
@@ -19,12 +31,18 @@ $('.portfolio').click(function(e) {
 	$('.Page1').show();
 	$('.portfolio').css("font-weight", "Bold");
 	$('.about').css("font-weight", "normal");
+	TweenLite.from('.Portfolio_Container', 1, {autoAlpha:0, delay:.5});
+	TweenMax.staggerFrom('header', 1, {scale:0, autoAlpha:0}, 0.2);
+	TweenMax.staggerFrom('.AboutPortfolio', 1, {scale:0, autoAlpha:0}, 0.2);
 });
 
 $('.next').click(function(e){
- $('.gallery-wrapper').css("left", "-=600px");
+ //$('.gallery-wrapper').css("left", "-=600px");
+ TweenMax.to('.gallery-wrapper', 0.7, {left:'-=600px', ease:Back.easeOut});
 });
 
 $('.previous').click(function(e){
- $('.gallery-wrapper').css("left", "+=600px");
+ //$('.gallery-wrapper').css("left", "+=600px");
+ TweenMax.to('.gallery-wrapper', 0.7, {left: '+=600px', ease:Back.easeOut});
 });
+
