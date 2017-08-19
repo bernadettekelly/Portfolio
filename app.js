932d3dc20e5b020a0721b851f60075eea2fac225
bernadettekelly <bernadettemkelly54@gmail.com>
2017-08-19 08:47:59 -0400

    responsive

--- app.js
+++ app.js
@@ -5,35 +5,61 @@ var tl = new TimelineLite();
 $(document).ready(function() {
 	$('.Page').hide();
 	$('.Page1').show();
-	$('.portfolio').css("font-weight", "Bold");
+	$('.portfolio').css("color", "#333");
 	TweenLite.from('.Portfolio_Container', 1, {autoAlpha:0, delay:.5});
-	TweenMax.staggerFrom('header', 1, {scale:0, autoAlpha:0}, 0.2);
-	TweenMax.staggerFrom('.AboutPortfolio', 1, {scale:0, autoAlpha:0}, 0.2);
+	TweenMax.from('header', 1, {scale:0, autoAlpha:0}, 0.2);
+	TweenMax.from('.AboutPortfolio', 1, {scale:0, autoAlpha:0}, 0.2);
+	aboutListeners();
+	portfolioListeners();
 });
+
+function aboutListeners() {
+	$('.about').on('mouseover', function(e){
+		TweenLite.to(this, 0.7, {color: '#333'})
+	});
+	$('.about').on('mouseleave', function(e){
+		TweenLite.to(this, 0.7, {color: '#00d1b2'})
+	});
+}
+
+function portfolioListeners() {
+	$('.portfolio').on('mouseover', function(e){
+		TweenLite.to(this, 0.7, {color: '#333'})
+	});
+	$('.portfolio').on('mouseleave', function(e){
+		TweenLite.to(this, 0.7, {color: '#00d1b2'})
+	});
+}
 
 $('.about').click(function(e) {
 	e.preventDefault();
 	$('.Page').hide();
 	$('.Page2').show();
-	$('.about').css("font-weight", "Bold");
-	$('.portfolio').css("font-weight", "normal");
-	TweenMax.staggerFrom('header', 1, {scale:0, autoAlpha:0}, 0.2);
-	TweenMax.staggerFrom('.AboutPortfolio', 1, {scale:0, autoAlpha:0}, 0.2);
+	portfolioListeners();
+	$('.about').off('mouseover');
+	$('.about').off('mouseleave');
+	$('.about').css({"font-weight": "normal", color: "#333"});
+	$('.portfolio').css({"font-weight": "normal", color: "#00d1b2"});
+	TweenMax.from('header', 1, {scale:0, autoAlpha:0});
 	TweenLite.from('.1', 1, {autoAlpha:0, delay:.25});
 	TweenLite.from('.2', 1, {autoAlpha:0, delay:.5});
 	TweenLite.from('.3', 1, {autoAlpha:0, delay:1});
 	TweenLite.from('.About_Title', 1, {autoAlpha:0, delay:.5});
+	TweenMax.from('.AboutPortfolio', 1, {scale:0, autoAlpha:0});
 });
 
 $('.portfolio').click(function(e) {
 	e.preventDefault();
 	$('.Page').hide();
 	$('.Page1').show();
-	$('.portfolio').css("font-weight", "Bold");
-	$('.about').css("font-weight", "normal");
+	aboutListeners();
+	$('.portfolio').off('mouseover');
+	$('.portfolio').off('mouseleave');
+	$('.portfolio').css({"font-weight": "normal", color: "#333"});
+	$('.about').css({"font-weight": "normal", color: "#00d1b2"});
 	TweenLite.from('.Portfolio_Container', 1, {autoAlpha:0, delay:.5});
-	TweenMax.staggerFrom('header', 1, {scale:0, autoAlpha:0}, 0.2);
-	TweenMax.staggerFrom('.AboutPortfolio', 1, {scale:0, autoAlpha:0}, 0.2);
+	TweenMax.from('header', 1, {scale:0, autoAlpha:0.5});
+	TweenMax.from('.AboutPortfolio', 1, {scale:0, autoAlpha:0});
 });
 
 $('.next').click(function(e){
